e2d156d2c488e62fd53a3aa8e95d3b521f656cb0
irgendeinDave <80263819+irgendeinDave@users.noreply.github.com>
2021-04-24 12:10:18 +0200

    Add PDF support and editing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include <QFileDialog>
 #include <QFile>
 #include <QMessageBox>
+#include <QtPrintSupport/QtPrintSupport>
+#include <QKeyEvent>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -12,15 +14,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     this->setCentralWidget(ui->textEdit);
 
-    //edit colors
-    this->setStyleSheet("background-color : #131212;");
-    ui->textEdit->setTextColor(qRgb(255, 255, 255));
+    MainWindow::setWindowTitle("INTELLIpad");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    ui = nullptr;
 }
 
 void MainWindow::on_actionOpen_triggered()
@@ -48,13 +47,22 @@ void MainWindow::on_actionOpen_triggered()
 void MainWindow::on_actionSave_triggered()
 {
     //open target file
-    QString filename = currentFile;
+    QString filename;
+    if (currentFile == "")
+    {
+        filename = QFileDialog::getSaveFileName(nullptr, "Save", "Select file!");
+    }
+    else
+    {
+        filename = currentFile;
+    }
     QFile file(filename);
     if (!file.open(QIODevice::WriteOnly | QFile::Text))
     {
         QMessageBox::warning(this, "Warning", "Cannot save into file: ", file.errorString());
         return;
     }
+    //save
     QTextStream save(&file);
     QString textToSave = ui->textEdit->toPlainText();
     save << textToSave;
@@ -65,14 +73,100 @@ void MainWindow::on_actionSave_triggered()
 void MainWindow::on_actionSave_As_triggered()
 {
     //open target file
-    QString filename = QFileDialog::getSaveFileName(nullptr, "Save", "Enter Filename");
+    QString filename = QFileDialog::getSaveFileName(nullptr, "Save", "Select file");
     QFile file(filename);
     if (!file.open(QIODevice::WriteOnly | QFile::Text))
     {
         QMessageBox::warning(this, "Warning", "Cannot save into file: ", file.errorString());
         return;
     }
+    //save
     QTextStream save(&file);
     QString textToSave = ui->textEdit->toPlainText();
     save << textToSave;
+}
+
+//save and quit
+void MainWindow::on_actionSave_and_Quit_2_triggered()
+{
+    //open target file
+    QString filename;
+    if (currentFile == "")
+    {
+        filename = QFileDialog::getSaveFileName(nullptr, "Save", "Select file!");
+    }
+    else
+    {
+        filename = currentFile;
+    }
+
+
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save into file: ", file.errorString());
+    }
+    //save
+    QTextStream save(&file);
+    QString textToSave = ui->textEdit->toPlainText();
+    save << textToSave;
+
+    close();
+}
+
+//print to pdf
+void MainWindow::on_actionPrint_to_PDF_triggered()
+{
+    ui->textEdit->setStyleSheet("color : Black;");
+
+    QPrinter printer;
+    QPrintDialog dialog(&printer, this);
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    if (dialog.exec() == QDialog::Rejected)
+    {
+        QMessageBox::warning(this, "Warning!", "Printing rejected!");
+    }
+    else
+    {
+        ui->textEdit->print(&printer);
+    }
+    ui->textEdit->setStyleSheet("color : White;");
+
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile = "";
+    ui->textEdit->setText("");
+    MainWindow::setWindowTitle("INTELLIpad");
+}
+
+void MainWindow::on_actionQuit_triggered()
+{
+    close();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
 }
