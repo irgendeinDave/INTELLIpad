37de04d89361cce92a71cc1b86eac5510cc8f5ad
irgendeinDave <80263819+irgendeinDave@users.noreply.github.com>
2021-07-14 20:12:13 +0200

    Update installer.sh

--- INTELLIpad/installer.sh
+++ INTELLIpad/installer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo Setting up INTELLIpad for you!
+echo Setting up INTELLIpad!
 
 #set up dictionaries in ~/.INTELLIpad
 origin=$(pwd)
 mkdir ~/.INTELLIpad
 cd ~/.INTELLIpad
 touch activeDic
-echo .INTELLIpad/usa2.txt
+echo .INTELLIpad/usa2.txt > ~/.INTELLIpad/activeDic
 cd origin
 mv *.txt ~/.INTELLIpad
 
+sudo chmod +x INTELLIpad
 sudo mv INTELLIpad /usr/bin
